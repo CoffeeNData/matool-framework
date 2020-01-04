02f4cfa700213fa23551c70744ffa1cba7bcd2dc
Reshi <51478329+CoffeeNData@users.noreply.github.com>
2020-01-04 06:27:18 -0500

    Fixed Syntax Error
    Ready for 1.10 release!

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -1,22 +1,6 @@
 #! /bin/bash
 #This uninstalls MatooL. Please dont :(
-del_matool(){
-    echo "Uninstalling MatooL..."
-    sudo rm -r /bin/matool
-    sudo rm -r ~/.matool-documents
-    echo "Done. You made me cry..."
-}
-
-#This is the main program. This just asks you if you really wanna uninstall this awesome script.
-echo "Are you sure you want to uninstall this awesome tool? [Y/N]"
-read answer
-if [$answer=="Y"]; then
-    del_matool()
-elif [$answer=="y"]; then
-    del_matool()
-elif [$answer=="yes"]; then
-    del_matool()
-elif [$answer=="YES"]; then
-    del_matool()
-else
-    echo "Phew. Aborting..."
+echo "Uninstalling MatooL..."
+sudo rm -r /bin/matool
+sudo rm -r ~/.matool-documents
+echo "Done. You made me cry..."
